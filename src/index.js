2b233c6abfb67eafb629fce37f241157c75a2668
drmanasa18.rao@gmail.com <drmanasa18.rao@gmail.com>
2020-09-22 15:27:22 +0200

    adds changes in code

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchMovies from "./searchMovies";
   
 class App extends React.Component{
   render(){
     return(
       <div className="container">
         <h1 className="title">React Movie Search</h1>
-        <searchmovies/>
+        <SearchMovies/>
       </div>
     )
   }
 }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+
 
 
