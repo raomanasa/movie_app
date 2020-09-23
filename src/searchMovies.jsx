d375606b62d540b7d4c41b159aea1fb32c433efb
drmanasa18.rao@gmail.com <drmanasa18.rao@gmail.com>
2020-09-23 13:24:37 +0200

    adds search function

--- src/searchMovies.jsx
+++ src/searchMovies.jsx
@@ -1,6 +1,23 @@
 import React from "react";
 
 export default function SearchMovies(){
+  const searchmovies = async (e) => {
+    e.preventDefault();
+
+    const query = "Jurassic Park";
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=d693ada1dd3255393b2a61dedf1fe9e3&language=en-US&query=${query}&page=1&include_adult=false`;
+     
+
+    try{
+      const res = await fetch(url);
+      const data  = await res.json();
+      console.log(data);
+  }catch(err){
+      console.error(err);
+  }
+    }
+
     return (
         <form className="form">
             <label className="label" htmlFor="query">Movie Name</label>
